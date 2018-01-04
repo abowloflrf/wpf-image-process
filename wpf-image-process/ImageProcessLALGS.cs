7daf0a6a6287c88c5e44cce3c42e40ca71635a0b
abowloflrf <lrf0829@126.com>
2018-01-04 19:03:00 +0800

    Add project files.

---
+++ wpf-image-process/ImageProcessLALGS.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Drawing;
+
+namespace wpf_image_process
+{
+    class ImageProcessLALGS
+    {
+        static public Bitmap FilterRed(Bitmap bitmap)
+        {
+            int x, y;
+
+            // 循环选择像素点然后重置每一个像素点
+            for (x = 0; x < bitmap.Width; x++)
+            {
+                for (y = 0; y < bitmap.Height; y++)
+                {
+                    Color pixelColor = bitmap.GetPixel(x, y);
+                    Color newColor = Color.FromArgb(pixelColor.R, 0, 0);
+                    bitmap.SetPixel(x, y, newColor);
+                }
+            }
+
+            return bitmap;
+        }
+
+        static public Bitmap FilterBlur(Bitmap bitmap)
+        {
+            int x, y;
+            for (x = 1; x < bitmap.Width-1; x++)
+            {
+                for (y = 1; y < bitmap.Height-1; y++)
+                {
+                    Color pixelColor1 = bitmap.GetPixel(x-1, y-1);
+                    Color pixelColor2 = bitmap.GetPixel(x, y-1);
+                    Color pixelColor3 = bitmap.GetPixel(x+1, y-1);
+                    Color pixelColor4 = bitmap.GetPixel(x-1, y);
+                    Color pixelColor5 = bitmap.GetPixel(x, y);
+                    Color pixelColor6 = bitmap.GetPixel(x+1, y);
+                    Color pixelColor7 = bitmap.GetPixel(x-1, y+1);
+                    Color pixelColor8 = bitmap.GetPixel(x, y+1);
+                    Color pixelColor9 = bitmap.GetPixel(x-1, y+1);
+
+                    int nr = (pixelColor1.R + pixelColor2.R + pixelColor3.R + pixelColor4.R + pixelColor6.R + pixelColor7.R + pixelColor8.R + pixelColor9.R) / 8;
+                    int ng = (pixelColor1.G + pixelColor2.G + pixelColor3.G + pixelColor4.G + pixelColor6.G + pixelColor7.G + pixelColor8.G + pixelColor9.G) / 8;
+                    int nb = (pixelColor1.B + pixelColor2.B + pixelColor3.B + pixelColor4.B + pixelColor6.B + pixelColor7.B + pixelColor8.B + pixelColor9.B) / 8;
+                    Color newColor = Color.FromArgb(nr, ng, nb);
+
+                    bitmap.SetPixel(x, y, newColor);
+                }
+            }
+
+            return bitmap;
+        }
+        static public Bitmap Contrast(Bitmap bitmap,float ratio)
+        {
+            int x, y;
+            double r = (100.0 + ratio) / 100.0;
+            for (x = 0; x < bitmap.Width; x++)
+            {
+                for (y = 0; y < bitmap.Height; y++)
+                {
+                    Color pixelColor = bitmap.GetPixel(x, y);
+                    int newR = Convert.ToInt32((((pixelColor.R / 255.0 - 0.5) * r + 0.5)) * 255);
+                    int newG = Convert.ToInt32((((pixelColor.G / 255.0 - 0.5) * r + 0.5)) * 255);
+                    int newB = Convert.ToInt32((((pixelColor.B / 255.0 - 0.5) * r + 0.5)) * 255);
+
+                    //处理RGB值越界
+                    if (newR < 0)
+                        newR = 0;
+                    else if (newR > 255)
+                        newR = 255;
+
+                    if (newB < 0)
+                        newB = 0;
+                    else if (newB > 255)
+                        newB = 255;
+
+                    if (newG < 0)
+                        newG = 0;
+                    else if (newG > 255)
+                        newG = 255;
+
+
+                    bitmap.SetPixel(x, y, Color.FromArgb(newR, newG, newB));
+                }
+            }
+
+            return bitmap;
+        }
+        static public Bitmap FaceWhiten(Bitmap bitmap)
+        {
+            int x, y;
+            for (x = 1; x < bitmap.Width - 1; x++)
+            {
+                for (y = 1; y < bitmap.Height - 1; y++)
+                {
+
+                    Color pixelColor = bitmap.GetPixel(x, y);
+                    int oldR = pixelColor.R;
+                    int oldG = pixelColor.G;
+                    int oldB = pixelColor.B;
+                    if (oldR > oldG && oldG > oldB && Math.Abs(oldR-oldG)>30)
+                    {
+                        int brighteness = 30;
+
+                        int newR = Convert.ToInt32((((pixelColor.R / 255.0 - 0.5) * 1.2 + 0.5)) * 255) + brighteness;
+                        int newG = Convert.ToInt32((((pixelColor.G / 255.0 - 0.5) * 1.2 + 0.5)) * 255) + brighteness;
+                        int newB = Convert.ToInt32((((pixelColor.B / 255.0 - 0.5) * 1.1 + 0.5)) * 255) + brighteness;
+
+
+
+                        //处理RGB值越界
+                        if (newR < 0)
+                            newR = 0;
+                        else if (newR > 255)
+                            newR = 255;
+
+                        if (newB < 0)
+                            newB = 0;
+                        else if (newB > 255)
+                            newB = 255;
+
+                        if (newG < 0)
+                            newG = 0;
+                        else if (newG > 255)
+                            newG = 255;
+
+                        Color newColor = Color.FromArgb(newR, newG, newB);
+                        bitmap.SetPixel(x, y, newColor);
+                    }
+
+
+                    
+                }
+            }
+
+            return bitmap;
+        }
+    }
+}
---
+++ wpf-image-process/MainWindow.xaml.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.IO;
+using System.Windows;
+using System.Windows.Forms;
+using System.Windows.Media.Imaging;
+using System.Drawing;
+
+namespace wpf_image_process
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        String imgToProcessPath = null;
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void button_process_Click(object sender, RoutedEventArgs e)
+        {
+
+            info_text.Text = "正在处理...";
+
+            //if(this.imgToProcessPath==null)
+            //{
+            var encoder = new PngBitmapEncoder();
+            encoder.Frames.Add(BitmapFrame.Create((BitmapSource)this.img_1.Source));
+            FileStream stream = new FileStream("temp", FileMode.Create);
+            encoder.Save(stream);
+     
+            this.imgToProcessPath = "temp";
+
+            stream.Dispose();
+            //}
+            
+
+            //Console.WriteLine("You have selected:" + select_box.SelectionBoxItem);
+            Bitmap bitmap = new Bitmap(this.imgToProcessPath);
+
+            int s = select_box.SelectedIndex;
+            switch (s)
+            {
+                case 0: 
+                    bitmap = ImageProcessLALGS.FilterRed(bitmap);
+                    break;
+                case 1:
+                    bitmap = ImageProcessLALGS.FilterBlur(bitmap);
+                    break;
+                case 2:
+                    bitmap = ImageProcessLALGS.Contrast(bitmap, 50);
+                    break;
+                case 3:
+                    bitmap = ImageProcessLALGS.Contrast(bitmap, -50);
+                    break;
+                case 4:
+                    bitmap = ImageProcessLALGS.FaceWhiten(bitmap);
+                    break;
+            }
+
+
+            img_2.Source = BitmapToBitmapImage(bitmap);
+            bitmap.Dispose();
+            info_text.Text = "处理完成！";
+
+
+        }
+
+        private void choose_button_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "Image Files (*.jpg, *.jpeg, *.png) | *.jpg; *.jpeg; *.png";
+
+            System.Windows.Forms.DialogResult result = dialog.ShowDialog();
+
+            if (result == System.Windows.Forms.DialogResult.OK)
+            {
+                Console.WriteLine("File:" + dialog.FileName);
+                //this.imgToProcessPath = dialog.FileName;
+                img_1.Source = new BitmapImage(new Uri(dialog.FileName));
+            }
+
+            
+
+        }
+
+        private void button_save_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog dialog = new SaveFileDialog();
+            dialog.Filter= "Image Files (*.jpg, *.jpeg, *.png) | *.jpg; *.jpeg; *.png";
+            //默认保存到我的文档图片目录下
+            dialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures);
+            dialog.FileName = "未命名.png";
+            if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                var encoder = new PngBitmapEncoder();
+                encoder.Frames.Add(BitmapFrame.Create((BitmapSource)this.img_2.Source));
+                using (FileStream stream = new FileStream(dialog.FileName, FileMode.Create))
+                    encoder.Save(stream);
+            }
+        }
+
+
+
+
+
+
+
+        private BitmapImage BitmapToBitmapImage(Bitmap bitmap)
+        {
+            BitmapImage bitmapImage = new BitmapImage();
+            MemoryStream ms = new MemoryStream();
+                bitmap.Save(ms, bitmap.RawFormat);
+                bitmapImage.BeginInit();
+                bitmapImage.StreamSource = ms;
+                bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
+                bitmapImage.EndInit();
+                bitmapImage.Freeze();
+            ms.Dispose();
+            return bitmapImage;
+        }
+    }
+}
